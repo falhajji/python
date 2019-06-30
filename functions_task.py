22489361d347de46e2f76afb90a5827899e64efd
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-06-30 20:56:39 +0300

    fixed the file

---
+++ functions_task.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+today = date.today()
+
+def check_birthdate(year, month, day):
+	if check_birthdate > today:
+		print ("True")
+	elif check_birthdate < today:
+		print ("False")
+
+
+
+
+year=input("Please enter year of birth: ")
+month=input("Please enter month of birth: ")
+day=input("Please enter day of birth: ")
