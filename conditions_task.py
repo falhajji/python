a804c41daf1c166c2cc8a542da337ce9e58447f5
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-02 18:58:42 +0300

    fourth commit with this task

--- conditions_task.py
+++ conditions_task.py
@@ -1,16 +1,11 @@
 print()
 print()
-print ("This is a simple calculator")
-print()
-print()
+print ("This is a simple calculator \n \n")
 number1= input("Enter the first number: ")
 number2= input("Enter the second number: ")
 
-print ()
-print ()
-print ()
-print ()
 if number1.isnumeric() and number2.isnumeric():
+# alternatively you can check if the input is a number using number1.isdigit() and number2.isdigit()
 	operation= input("Choose the operation (+, -, /, *): ")
 
 	if operation=="+":
@@ -26,10 +21,10 @@ if number1.isnumeric() and number2.isnumeric():
 		answer = int(number1) / int(number2)
 		print ("The answer is %s" % answer)
 	else:
-		print("The operation entered is not valid")
+		print("The operation entered is not valid, please try again.")
 
 
 else:
-	print ("One of the numbers entered is not valid")
+	print ("One of the numbers entered is not valid, please try again.")
 
 
