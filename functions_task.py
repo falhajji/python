99dc9d025f13d74dc0caa34d6989363d6ce7f506
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-02 12:56:01 +0300

    first commit to test functions

--- functions_task.py
+++ functions_task.py
@@ -1,16 +1,45 @@
-from datetime import date
+from datetime import datetime
 
-today = date.today()
+
+#userdate=datetime.(year, month, day)
+#or you can use today = datetime.now()
+#birthdat
 
 def check_birthdate(year, month, day):
-	if check_birthdate > today:
-		print ("True")
-	elif check_birthdate < today:
-		print ("False")
+	today = datetime.now()
+	DOB = datetime(year, month, day)
+	#DOB has to be defined here within the function so the code can recognise the paramters of year month day)
+	if DOB < today:
+		return True
+		
+	else:
+		return False
+
+def calculate_age(year, month, day):
+	today = datetime.now()
+	DOB = datetime(year, month, day)
+
+	Age = today - DOB
+	AgeDays = Age.days
+#we do this step so that we get the 'days' 
+
+	AgeYear = int(AgeDays/365)
+	AgeDay = AgeDays%365
+#this will give us number of days since (our) last birthday
+
+	AgeMonth = int(AgeDays/30)
 
 
 
+	print ("You are %d years, %d months, and %d days" % (AgeYear, AgeMonth, AgeDay))
 
-year=input("Please enter year of birth: ")
-month=input("Please enter month of birth: ")
-day=input("Please enter day of birth: ")
+
+
+year=int(input("Please enter year of birth: "))
+month=int(input("Please enter month of birth: "))
+day=int(input("Please enter day of birth: "))
+
+if check_birthdate(year, month, day) == True:
+ 	calculate_age(year, month, day)
+else:
+	print ("The birthdate entered is not valid. This is real life, not \'Back From The Future\'.")
