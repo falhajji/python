740fbc1f533ec247c4564e783e095fbe3116d9b9
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-02 19:11:37 +0300

    fifth commit with this task after Mshary confused us in class ;p

--- conditions_task.py
+++ conditions_task.py
@@ -8,7 +8,8 @@ if number1.isnumeric() and number2.isnumeric():
 # alternatively you can check if the input is a number using number1.isdigit() and number2.isdigit()
 	operation= input("Choose the operation (+, -, /, *): ")
 
-	if operation=="+":
+	total = ""
+	#if operation=="+":
 		answer = int(number1) + int(number2)
 		print ("The answer is %s" % answer)
 	elif operation=="-":
@@ -23,6 +24,8 @@ if number1.isnumeric() and number2.isnumeric():
 	else:
 		print("The operation entered is not valid, please try again.")
 
+	#print ("The answer is %s" % answer)
+	#^^^ using this method (and line 11 where we define total as empty), saves us from repeating the same code under every if and elif
 
 else:
 	print ("One of the numbers entered is not valid, please try again.")
