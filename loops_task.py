a4badc388ddbe9e25fc5251ad85af1fe4f299672
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-04 17:37:21 +0300

    second commit with loops task, adding extra notes from class

--- loops_task.py
+++ loops_task.py
@@ -9,11 +9,13 @@ item=""
 # the above code is the same as whats below and will work too!
 while item != "done":
 	item = input("Please enter an item (enter \"done\" when finished): ")
+	
 	if item == "done":
 		break
 #^^without this line of code, the loop will continue, even when the user enters "done!"
 	price = float(input("Price: "))
 	quantity = int(input("Quantity: "))
+	
 	user_items = {
 	"item" : item,
 	"price" : price,
@@ -27,10 +29,13 @@ print ("------------------- \nReceipt\n-------------------")
 total = 0
 
 for x in shoppinglist:
-#here we redefine the List shopping list to "x"
+#here we ask the code to loop through the shopping list, which will return the dictionary stored inside the list.
 	print ("%s %s %.3f K.D." % (x["quantity"], x["item"], x["price"]*x["quantity"]))
+#Had we not converted the strings into the correct value type (float and integer) in line 16/17 when defining the variables price and quantity,
+#... we can do that here at the end of the formula above where print ("%s %s %.3f K.D." % (x["quantity"], x["item"], float(x["price"])*int(x["quantity"]))
+
 	total = total + x["price"]*x["quantity"]
-	#
+	
 	# Keep in mind that the key in your dictionary is always a String! i.e. use ""
 
 #total = 
